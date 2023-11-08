588507e2acc5946b773a1a8d43c6a8f5cd024547
audiBookning <audiobookning@gmail.com>
2023-11-08 15:14:42 +0000

    init

---
+++ addons/toggle_external_editor/toggle_external_editor.gd
@@ -0,0 +1,49 @@
+@tool
+extends EditorPlugin
+
+""" 
+
+settings for GDScript at
+	text_editor/external/use_external_editor
+settings for NET at
+	dotnet/editor/external_editor
+
+dotnet/editor/external_editor is a select widget wityh at the time i am writing this
+	(no way that i can find to programmatically get the keys of the setting...):
+	0 - disabled
+	1 - visual studio
+	2 - monodevelop
+	3 - visual studio code
+	4 - jet brain rider
+	5 - custom
+Note that +1 must be added to the index when setting the value
+for example, for vs code we have
+settings.set("dotnet/editor/external_editor", 3+1)
+
+""" 
+
+var checkbox = CheckBox.new()
+var settings = get_editor_interface().get_editor_settings()
+
+func _enter_tree():
+	checkbox.text = "Use External Editor"
+	checkbox.set_pressed(settings.get("dotnet/editor/external_editor"))
+   
+	#checkbox.set_pressed(settings.get("text_editor/external/use_external_editor"))
+	checkbox.connect("toggled", Callable(self, "_on_CheckBox_toggled"))
+	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, checkbox)
+
+func _exit_tree():
+	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, checkbox)
+	checkbox.free()
+
+func _on_CheckBox_toggled(pressed):
+	# maintain this line to get vs code to work with both gds and c#
+	settings.set("text_editor/external/use_external_editor", pressed)
+	if pressed:
+		# 4 for Visual Studio Code
+		settings.set("dotnet/editor/external_editor", 4)
+	else:
+		settings.set("dotnet/editor/external_editor", 0)
+		
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Toggle External Editor
+
+Just a little addon to toggle on/off the use of an external editor. It is useful in certain use cases.
+It is aimed at the NET version of Godot 4 and c#, but it also works for GDScript.
+
+
+## Disclaimer
+
+There is no intention in turning this in any sort of maintained project. Anyone is welcome to copy or fork it to do whatever they want with it.
